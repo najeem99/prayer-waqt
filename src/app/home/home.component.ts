41781c6712887a67776fe011cafa8868345f0f30
najeemfasil <najeemfasil1@gmail.com>
2023-05-14 23:45:50 +0400

    adding time and css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,13 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+   prayerTimes:any = []
    today: number = Date.now();
-
+    i = 0
   constructor() {
-      setInterval(() => {this.today = Date.now()}, 1);
+      setInterval(() => {this.today = Date.now()
+        this.prayerTimes.forEach((element:any,index:number) => {
+          if(index == this.i){
+            element.isUpcoming = true
+          }else{
+            element.isUpcoming = false
+          }
+        });
+      this.i = this.i  + 1;
+      if(this.i == 5)
+            this.i=0;
+      }, 1000);
   }
   ngOnInit(): void {
+    this.prayerTimes = [
+      {
+        prayerTimeName:"Fajr",
+        prayerTime:"10:25",
+        isUpcoming:false,
+      },
+      {
+        prayerTimeName:"Sunrise",
+        prayerTime:"10:25",
+        isUpcoming:false,
+      },
+      {
+        prayerTimeName:"Dhuhr",
+        prayerTime:"10:25",
+        isUpcoming:false,
+      },
+      {
+        prayerTimeName:"Asr",
+        prayerTime:"10:25",
+        isUpcoming:false,
+      },
+      {
+        prayerTimeName:"Maghrib",
+        prayerTime:"10:25",
+        isUpcoming:false,
+      },
+      {
+        prayerTimeName:"Isha",
+        prayerTime:"10:25",
+        isUpcoming:true,
+      }
+    ]
   }
 
 }
