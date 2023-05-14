f2de4439c67d2ea9ff0edc698ff9dc9f73966e0b
najeem99 <najeemfasil1@gmail.com>
2023-05-14 13:23:51 +0400

    initial changes p1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+   today: number = Date.now();
 
+  constructor() {
+      setInterval(() => {this.today = Date.now()}, 1);
+  }
   ngOnInit(): void {
   }
 
